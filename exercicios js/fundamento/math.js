5e8a51353cfb4bf2a484b8971d9de5956e0e1bf7
Deloam <deyvisondn@gmail.com>
2020-04-14 12:42:23 -0300

    aula sobre a função Math em JS

---
+++ exercicios js/fundamento/math.js
@@ -0,0 +1,11 @@
+// calculando a area de uma circunferência em JS
+const raio = 5.6
+
+const area = Math.PI * Math.pow(raio, 2)
+
+console.log(area)
+console.log(typeof Math)
+
+a = 3
+
+console.log(a)
