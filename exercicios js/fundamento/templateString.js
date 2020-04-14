2e4ac989262f7ee13f0bac5e0e53290a44b12720
Deloam <deyvisondn@gmail.com>
2020-04-14 19:15:48 -0300

    aula sobre templates em string e alguns usos

---
+++ exercicios js/fundamento/templateString.js
@@ -0,0 +1,18 @@
+// como usar template string
+const nome = 'Rebeca'
+
+const concatenacao = 'Olá ' + nome + '!'
+
+// uso de template
+const template = ` 
+    Olá
+    ${nome}!`
+
+console.log(concatenacao, template) // template considera quebra de linha
+
+//expressoes
+console.log(`1 + 1 = ${1 + 1}`) // dentro do ${} pode fazer operações
+
+const up = texto => texto.toUpperCase() // passando uma função para uma variavel string
+
+console.log(`Ei.... ${up('cuidado')}!`) // faz cuidado ficar em maiúsculo
