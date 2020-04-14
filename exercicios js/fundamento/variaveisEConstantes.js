f88804bd2fe833faab84170c61d3365df6b793e3
Deloam <deyvisondn@gmail.com>
2020-04-14 00:41:53 -0300

    arquivo de variaveis e constantes

---
+++ exercicios js/fundamento/variaveisEConstantes.js
@@ -0,0 +1,17 @@
+//isso é uma variavel usando a palavra reservada VAR
+var a = 3
+
+//isso é uma variavel usando a palavra reservada LET
+// ** melhor usar LET do que VAR
+let b = 4
+
+var a = 30
+
+b = 40
+
+console.log(a, b)
+
+//isso é uma contante em js
+const c = 5
+
+console.log(c)
