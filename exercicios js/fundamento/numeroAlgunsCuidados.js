81fc2e3a3bbdbe875f6d715343b9bcec213769f2
Deloam <deyvisondn@gmail.com>
2020-04-14 11:53:14 -0300

    aula sobre numero e cuidados com eles em JS

---
+++ exercicios js/fundamento/numeroAlgunsCuidados.js
@@ -0,0 +1,11 @@
+console.log(7 / 0) // retorna função infinito, pois nao pode dividir por zero
+
+console.log("10" / 2) // retorna 5 pois JS entendo que 10 em string pode ser convertido em inteiro
+
+console.log("Show!" * 2) // não imprimi no console 2x a palavra show igual em outras linguagem, retorna um erro Not Num
+
+console.log(0.1 + 0.7) // não retorn 0.8 e sim 0.7999999999 (é um tipo de espeficicação do W3C)
+
+// console.log(10.toString()) // não funciona
+
+console.log((10.345).toFixed(2)) // consegue chamar literal dentro de parenteses
