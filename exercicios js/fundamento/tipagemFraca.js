be0de5d873134c79488c0a57ca8792d9c6a048e4
Deloam <deyvisondn@gmail.com>
2020-04-14 00:49:03 -0300

    Aula sobre tipagem fraca em js

---
+++ exercicios js/fundamento/tipagemFraca.js
@@ -0,0 +1,17 @@
+//uso de tipagem, mudando o tipo da variavel
+
+let qualquer = 'legal'
+
+console.log(qualquer)
+console.log(typeof qualquer)
+
+qualquer = 3.1516
+console.log(qualquer)
+console.log(typeof qualquer)
+
+//evitar nome generico e siglas para variaveis
+// exemplos de como nao usar
+
+let valor = ''
+let numero = 1
+let pqp = false // Produto quimico perigoso
