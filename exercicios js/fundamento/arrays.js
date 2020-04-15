f260e83e1114ada1561508e73b06d5e890c5d434
Deloam <deyvisondn@gmail.com>
2020-04-14 22:04:47 -0300

    aula sobre arrays e alguns metodos sobre arrays

---
+++ exercicios js/fundamento/arrays.js
@@ -0,0 +1,24 @@
+// aula sobre array em js
+const valores = [7.7, 8.9, 6.3, 9.2]
+
+console.log(valores[0], valores[3])
+
+console.log(valores[4])
+
+valores[4] = 10
+
+console.log(valores)
+console.log(valores.length) // quantos elementos tem no array
+
+
+//arrays com multiplos valores (de mais um tipo)
+valores.push({id: 3}, false, null, 'teste')
+console.log(valores)
+
+console.log(valores.pop())
+
+delete valores[0] // deixa vazio o valor do indice escolhido
+
+console.log(valores)
+
+console.log(typeof valores)// mostra que é do tipo object
