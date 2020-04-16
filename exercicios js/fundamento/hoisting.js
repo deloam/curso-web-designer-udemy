e698b710b992fd4e90c0830446c551d6917b7e96
Deloam <deyvisondn@gmail.com>
2020-04-16 12:03:38 -0300

    aula sobre hoisting em JS

---
+++ exercicios js/fundamento/hoisting.js
@@ -0,0 +1,7 @@
+console.log('a =', a) // => a = undefined // variavel existe, mas nao foi setado valor a ela
+var a = 2
+console.log('a =', a) // => a = 2
+
+// com LET  nao ocorre esse issamento, levar para cima do código a declaração da variavel
+
+
