710b0b7b558cdf00280cee5078c8aa8215000612
Deloam <deyvisondn@gmail.com>
2020-04-20 22:27:41 -0300

    aula sobre notação ponto em JS

---
+++ exercicios js/fundamento/notacaoPonto.js
@@ -0,0 +1,9 @@
+console.log(Math.ceil(6.1))
+
+const obj1 = {}
+obj1.nome = 'Bola' //atribuindo dinamicamente atributos no objeto
+
+// obj1['nome'] = 'Bola2' // outra forma de atribuir atributos no objeto
+
+console.log(obj1.nome)  
+
