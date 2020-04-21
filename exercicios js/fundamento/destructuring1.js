c935c10082da063a613ade04b8078bcd1a340113
Deloam <deyvisondn@gmail.com>
2020-04-20 22:27:26 -0300

    Aula sobre destructurin em JS

---
+++ exercicios js/fundamento/destructuring1.js
@@ -0,0 +1,20 @@
+// aula sobre novo recurso do ES2015
+
+const pessoa = {
+    nome: 'Ana',
+    idade: 5,
+    endereco: {
+        logradouro: 'Rua ABC',
+        numero: 1000
+    }
+}
+
+const { nome, idade} = pessoa
+console.log(nome, idade)
+
+const { nome: n, idade: i} = pessoa
+console.log(n, i)
+
+const { endereco: { logradouro, numero, cep } } = pessoa
+
+console.log(logradouro, numero, cep) // cep ve como undefined
