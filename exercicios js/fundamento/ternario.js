16df91f5a6f2b8f2c9c1dfb70abfbec6afc89091
Deloam <deyvisondn@gmail.com>
2020-04-21 21:16:51 -0300

    aula sobre operador ternário

---
+++ exercicios js/fundamento/ternario.js
@@ -0,0 +1,8 @@
+// aula sobre operador ternario em JS
+
+
+const resultado = nota => nota >= 7 ? 'Aprovado' : 'Reprovado'
+
+console.log(resultado(7.1)) // => aprovado
+console.log(resultado(6.9)) // => reprovado
+
