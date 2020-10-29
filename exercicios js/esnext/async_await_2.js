21b5169e3899878e7753170ff5a87c04523c00bc
Deloam <deyvisondn@gmail.com>
2020-10-29 19:05:12 -0300

    ultimo codigo das aulas de javacript

--- exercicios js/esnext/async_await_2.js
+++ exercicios js/esnext/async_await_2.js
@@ -1,11 +1,34 @@
-function gerarNumeroEntre(min, max) {
-    if(min > max){
-        [max, min] = [min, max]
-    }
+function gerarNumeroEntre(min, max, numerosProibidos) {
+    if (min > max) [max, min] = [min, max]
 
-    return new Promise(resolve =>{
-        const fator = max - min +1
+    return new Promise((resolve, reject) => {
+        const fator = max - min + 1
         const aleatorio = parseInt(Math.random() * fator) + min
-        resolve(aleatorio)
+        if (numerosProibidos.includes(aleatorio)) {
+            reject('Numero repetido!')
+        } else {
+            resolve(aleatorio)
+        }
     })
 }
+
+async function gerarMegaSena(qtdNumeros, tentativas = 1) {
+    try {
+        const numeros = []
+        for (let _ of Array(qtdNumeros).fill()) {
+            //esperar ate conseguir os numeros
+            numeros.push(await gerarNumeroEntre(1, 60, numeros))
+        }
+        return numeros
+    } catch (error) {
+        if (tentativas > 100) {
+            throw "Não Deu certo"
+        } else {
+            return gerarMegaSena(qtdNumeros, tentativas + 1)
+        }
+    }
+}
+
+gerarMegaSena(25)
+    .then(console.log)
+    .catch(console.log)
